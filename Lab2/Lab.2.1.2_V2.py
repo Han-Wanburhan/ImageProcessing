0c9a6277ebaa5937c52446ae25b4ab3ee3a060d4
Han-Wanburhan <Hangster10761@gmail.com>
2023-08-14 00:55:35 +0700

    []

--- Lab2/Lab.2.1.2_V2.py
+++ Lab2/Lab.2.1.2_V2.py
@@ -11,31 +11,31 @@ def GammaEquation(img, gamma):
     return result_img.astype(np.uint8)
 
 img_1 = cv2.imread("./Lab2/img/Miso.jpg")
-image_resized = cv2.resize(img_1, (200, 200))
+# image_resized = cv2.resize(img_1, (200, 200))
 
 output_file = "./Lab2/video/lab_2.1.2_V2_output_file.mp4"
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 fps = 1
 
-vid_output = cv2.VideoWriter(output_file, fourcc, fps, (image_resized.shape[1], image_resized.shape[0]))
+vid_output = cv2.VideoWriter(output_file, fourcc, fps, (img_1.shape[1], img_1.shape[0]))
 
 Y_values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.11, 1.12, 1.13, 1.14, 1.15, 1.16, 1.17, 1.18, 1.19, 1.20]
 
 for gamma in Y_values:
-    adjusted_img = GammaEquation(image_resized, gamma)
+    adjusted_img = GammaEquation(img_1, gamma)
     vid_output.write(adjusted_img)
 
 vid_output.release()
 
 plt.subplot(1, 2, 1)
-plt.imshow(cv2.cvtColor(image_resized, cv2.COLOR_BGR2RGB))
+plt.imshow(cv2.cvtColor(img_1, cv2.COLOR_BGR2RGB))
 plt.title("Original")
 
 plt.subplot(1, 2, 2)
 plt.imshow(cv2.cvtColor(adjusted_img, cv2.COLOR_BGR2RGB))
 plt.title("Gamma Corrected")
 
-print(image_resized,"\n","*"*50)
+print(img_1,"\n","*"*50)
 print(adjusted_img)
 
 plt.show()
