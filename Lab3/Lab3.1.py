1e90b185cc75a664cdec29277da359c4f51fdae5
Han-Wanburhan <Hangster10761@gmail.com>
2023-09-04 16:01:43 +0700

    []

--- Lab3/Lab3.1.py
+++ Lab3/Lab3.1.py
@@ -46,10 +46,11 @@ print(listparam)
 # plt.show()
 
 # Plot the feature maps
+plt.figure(figsize=(12, 12))
 for i in range(feature_maps.shape[3]):
     plt.subplot(8, 8, i + 1)  # Adjust the subplot layout as needed
     plt.imshow(feature_maps[0, :, :, i],cmap='gray' )  # Display a single feature map
     plt.axis('off')
-    plt.title("3.1")
-
+    plt.title(f'IMG {i + 1}')
+plt.tight_layout()
 plt.show()
